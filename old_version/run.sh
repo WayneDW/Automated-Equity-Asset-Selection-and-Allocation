9792509b47bd0015a3ebb26acc6ceaf3586707ef
Wei Deng <greyman@live.cn>
2016-10-08 10:46:29 -0400

    change name

---
+++ old_version/run.sh
@@ -0,0 +1,17 @@
+# download ticker list from NASDAQ, get basic ticker information such as industry, vol
+
+# for each ticker, download historical prices from yahoo.finance
+
+# for each ticker, download key ratios from morning star
+
+
+## save data in this format
+
+BIDU, {industry:Technology, MarketCap: XXX, key_ratios:{dates:date1_date2_..., Revenue:xxx, }, prices:{date1:price1,date2:price2,...}, returns:{date1:price1, date2:price2, ...}}
+
+# feature data based on its recent price
+
+# take key ratios as input, use machine learning method to classify the companies into different groups
+
+
+
