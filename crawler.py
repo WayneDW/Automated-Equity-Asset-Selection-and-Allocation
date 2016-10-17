c246b60b6b71c299028027f993116e1a0eea1389
Wei Deng <greyman@live.cn>
2016-10-17 17:11:32 -0400

    download failed ticker list the 2nd time

--- crawler.py
+++ crawler.py
@@ -24,7 +24,8 @@ class stock_info_collector:
 		# if file existed, exit
 		fileName = "_".join(["raw", self.time_start, self.time_end, self.time_type])
 		if os.path.isfile("./dat/" + fileName): sys.exit("file already existed!")
-		fout = open("./dat/" + fileName, 'a+')
+
+		self.failedList, cnt = {}, 1
 		# get basic indicators associated with the ticker
 		for exchange in ["NASDAQ", "NYSE", "AMEX"]:
 			print "Download tickers from " + exchange
@@ -35,26 +36,58 @@ class stock_info_collector:
 			for num, line in enumerate(csv):
 				line = line.strip().strip('"').split('","')
 				if num == 0 or len(line) != 9: continue # filter unmatched format
-
+				#if cnt > 10: continue # test purpose
 				ticker, name, lastSale, MarketCap, IPOyear, sector, \
 				industry = line[0: 4] + line[5: 8]
-				repeat_times = 3
-				for _ in range(repeat_times): # repeat N times to download
-					try:
-						time.sleep(random.uniform(2, 3))
-						priceStr = self.yahoo_price(ticker)
-						ratioStr = self.key_ratios(ticker)
-						if priceStr and ratioStr: # skip loop if data fetched
-							break
-					except:
-						if _ == 0: print num, ticker, "Http error!"
-				if _ == repeat_times - 1: continue
-				print num, ticker
-				#print ratioStr, priceStr
-				fout.write( "{ticker:" + ticker + "," + "name:" + name + "," \
-				"lastSale:" + lastSale + "," + "MarketCap:" + MarketCap + "," \
-				"IPOyear:" + IPOyear + "," + "sector:" + sector + "," + "industry:" + \
-				industry + ',\n' + ratioStr + "\n" + priceStr + "}\n") 
+				tag, priceStr, ratioStr = self.repeatDownload(ticker)
+
+				if tag == 0: 
+					self.failedList[ticker] = [ticker, name, lastSale, MarketCap, \
+					IPOyear, sector, industry]
+					print ticker, "failed"; continue
+				print ticker, cnt
+				self.writeFile(fileName, ticker, name, lastSale, MarketCap, \
+					IPOyear, sector, industry, ratioStr, priceStr)
+				cnt += 1
+		self.downloadFailed()
+	
+	def writeFile(self, fileName, ticker, name, lastSale, MarketCap, \
+			IPOyear, sector, industry, ratioStr, priceStr):
+		fout = open("./dat/" + fileName, 'a+')
+		#print ratioStr, priceStr
+		fout.write( "{ticker:" + ticker + "," + "name:" + name + "," \
+		"lastSale:" + lastSale + "," + "MarketCap:" + MarketCap + "," \
+		"IPOyear:" + IPOyear + "," + "sector:" + sector + "," + "industry:" + \
+		industry + ',\n' + ratioStr + "\n" + priceStr + "}\n") 
+		fout.close()
+
+	def repeatDownload(self, ticker):
+		repeat_times = 3 # repeat download for N times
+		priceStr, ratioStr = "", ""
+		for _ in range(repeat_times): 
+			try:
+				time.sleep(random.uniform(2, 5))
+				priceStr = self.yahoo_price(ticker)
+				ratioStr = self.key_ratios(ticker)
+				if priceStr and ratioStr: # skip loop if data fetched
+					break
+			except:
+				if _ == 0: print ticker, "Http error!"
+		#tag = 0 if _ == repeat_times - 1 else 0 # decide if skip this loop
+		tag = 0 if len(priceStr) == 0 or len(ratioStr) == 0 else 1
+		return tag, priceStr, ratioStr
+
+	def downloadFailed(self):
+		print "start downloading failed ticker list", self.failedList.keys()
+		for num, ticker in enumerate(self.failedList):
+			tag, priceStr, ratioStr = self.repeatDownload(ticker)
+			print "2nd", num, ticker, tag
+			if tag == 0: continue
+			
+			ticker, name, lastSale, MarketCap, IPOyear, \
+				sector, industry = self.failedList[ticker]
+			self.writeFile(self, fileName, ticker, name, lastSale, MarketCap, \
+				IPOyear, sector, industry, ratioStr, priceStr)
 
 	# Classical indicators for fundamental analysis
 	# Data source: http://www.morningstar.com/
@@ -76,17 +109,19 @@ class stock_info_collector:
 			string, if_skip_row = [], 0
 			for item in res:
 				item = item.replace(',', '').replace('"', '')
-				if item == "": item = "nan" # label missing value as numpy None
-				if item in ["TTM", "Latest"] and num != 2: if_skip_row = 1 # tag redundant title rows
+				if item == "":
+					item = "nan" # label missing value as numpy None
+				if item in ["TTM", "Latest"] and num != 2: 
+					if_skip_row = 1 # tag redundant title rows
 				string.append(item)
 			if if_skip_row: continue # delete redundant rows
 			name = tag.replace(" ", "_") + name.replace(" ", "_")
 			#print ticker, name, ",".join(string)
-			standard_output += "key_ratios_" + name.replace("'", "") + ":" + " ".join(string) + ",\n"
-		return standard_output [:len(standard_output) - 1] # delete last char in the end of string
+			standard_output += "key_ratios_" + name.replace("'", "") \
+				+ ":" + " ".join(string) + ",\n"
+		return standard_output [:len(standard_output) - 1] # delete last char in the end 
 
 	# Return the historical stock price, date type -> monthly: m; weekly: w; daily: d
-	# http://chart.finance.yahoo.com/table.csv?s=BIDU&a=5&b=12&c=2016&d=6&e=12&f=2016&g=m&ignore=.csv
 	def yahoo_price(self, ticker):
 		# split date parameters
 		start_y, start_m, start_d = self.time_start.split("-")
@@ -99,7 +134,8 @@ class stock_info_collector:
 		# Construct url
 		url1 = "http://chart.finance.yahoo.com/table.csv?s=" + ticker
 		url2 = "&a=" + start_m + "&b=" + start_d + "&c=" + start_y
-		url3 = "&d=" + end_m + "&e=" + end_d + "&f=" + end_y + "&g=" + self.time_type + "&ignore=.csv"
+		url3 = "&d=" + end_m + "&e=" + end_d + "&f=" + end_y + \
+			"&g=" + self.time_type + "&ignore=.csv"
 
 		# parse url
 		response = urllib2.urlopen(url1 + url2 + url3)
