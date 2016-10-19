e2f9beffcebdcfb099ad613920299b54f71a64b0
Wei Deng <greyman@live.cn>
2016-10-18 23:10:46 -0400

    big improvement, add different scaling rules

--- feature_engineering.py
+++ feature_engineering.py
@@ -10,8 +10,8 @@ from sklearn.preprocessing import Imputer
 from collections import OrderedDict
 from collections import Counter
 
-# may have different time horizon
-# add sector industry, etc...
+# this script need a large amount of memory
+# haven't consider the industry information and marketCap
 
 class preprocessing:
 	def __init__(self, date_start, date_end, date_type):
@@ -27,16 +27,17 @@ class preprocessing:
 		
 	def loadDict(self):
 		print "Load feature dictionary"
-		f = open('./dat/feature_projection')
+		f = open('./input/feature_projection')
 		self.wordDict = {}
 		self.wordDictRev = {}
 		for num, line in enumerate(f):
-			self.wordDict[line.strip()] = num
-			self.wordDictRev[num] = line.strip()
+			feature, ftype = line.strip().split('\t')
+			self.wordDict[feature] = ftype
+			self.wordDictRev[num] = feature
 
 	def readFile(self):
 		print "Read Raw Data"
-		f = open('./dat/' + "_".join(["raw", date_start, date_end, date_type]))
+		f = open('./input/' + "_".join(["raw", date_start, date_end, date_type]))
 		res = ""
 		for i, line in enumerate(f):
 			res += line
@@ -45,33 +46,37 @@ class preprocessing:
 	def createFeature(self):
 		print "Create Feature Matrix"
 		# turn the feature in a (samples, features) matrix, A is a feature for one sample
-		self.feature, A = np.empty([0, 11 * 83]), np.empty([0, 11])
+		self.feature, A = np.empty([0, 11 * 80]), np.empty([0, 11])
 		self.tickerList = [] # use array, since we need the location of each ticker
 		self.stock_prices = {}# 2-D hash table
-		last, cnt = -1, 1
+		last, cnt, cnt_del = -1, 1, 1
 		for _, line in enumerate(self.lists): # read key-value pairs
-			#if _ > 1 * 10 ** 7: break # used to test large file
+			#if _ > 5 * 10 ** 6: break # used to test large file
 			dat = line.strip().split(":") # split key-value pair
 			if dat[0] == "ticker": # everytime update new ticker, clear past array
 				ticker = dat[1]
-				if np.shape(A) == (83, 11):
-					if last != -1 and len(self.stock_prices[last]) > 252 * 3 \
-						and years[0] == '2006': # date has different time horizon
-						A = A.flatten() # change 2-D matrix to 1-D
-						self.tickerList.append(last)
-						self.feature = np.vstack([self.feature, A])
-						print cnt, last; cnt += 1
-				A = np.empty([0, 11])
-				self.stock_prices[ticker] = {}
+				# in year 2016, most data starts in 2006
+				# the rest focus on 2007 (they just IPO in 2007, so 2007 has 2 data)
+				if last != -1 and years[0] in ['2006', '2007'] and np.shape(A) == (80, 11):
+					A = A.flatten() # change 2-D matrix to 1-D
+					self.tickerList.append(last)
+					self.feature = np.vstack([self.feature, A])
+					print cnt, last; cnt += 1
+				
+				A = np.empty([0, 11]) # reinitialize A
+				self.stock_prices[ticker] = {} # initialize stock price
 				last = ticker
 			if dat[0] == "key_ratios_Time":
 				years = np.array(dat[1].split(' '))
 				if len(years) == 11: self.time_horizon = years
 
-			if dat[0] in self.wordDict: # if key_ratios are these we need
-				numList = np.array(dat[1].split(' ')) # create one row in numpy
-				if len(numList) != np.shape(A)[1]: continue # in case format not match
-				A = np.vstack([A, numList]) # add to A
+			if dat[0].startswith("key_ratios_"): 
+				for currency in ["USD", "CNY", "GBP", "JPY", "EUR", "CAD", "CHF", "AUD", "HKD"]:
+					dat[0] = dat[0].replace("_" + currency, "") # unify currency
+				if dat[0] in self.wordDict: # if key_ratios are these we need
+					numList = np.array(dat[1].split(' ')) # create one row in numpy
+					if len(numList) != np.shape(A)[1]: continue # in case format not match
+					A = np.vstack([A, numList]) # add to A
 
 			m = re.search(r"(\d{4}\-\d{2}\-\d{2})_adjClose", dat[0]) # match stock price
 			if m:
@@ -80,7 +85,7 @@ class preprocessing:
 				self.stock_prices[ticker][curDate] = stock_price
 			
 		# add the last qualified sample
-		if np.shape(A) == (83, 11):
+		if years[0] in ['2006', '2007'] and np.shape(A) == (80, 11):
 			if last != -1 and len(self.stock_prices[last]) > 252 * 3: # data check
 				A = A.flatten() # change 2-D matrix to 1-D
 				self.tickerList.append(last)
@@ -88,8 +93,8 @@ class preprocessing:
 		self.feature = self.feature.astype(np.float)
 
 	def interpolate(self): # handle missing values (middle of data)
-		### one reason: SVM can't handle missing value (despite Boosting could) 
-		### another: we will move forward time window to make prediction       
+		# model can't handle missing value (despite Boosting could) 
+		# we will move forward time window to make prediction       
 		
 		print "Interpolate and predict missing values"
 		regr = linear_model.LinearRegression()
@@ -101,13 +106,8 @@ class preprocessing:
 				y = dataSets[num: num + 11]
 				newX = x[~np.isnan(y)] # ignore nan value
 				newY = y[~np.isnan(y)]
-				if len(newX) == 0 or len(newX) == 11:
+				if len(newX) < 5 or len(newX) == 11:
 					continue # data complete or too many missing values
-				elif len(newX) < 4: # too little data, use mean estimator
-					for _ in range(num, num + 11):
-						if _ in newX: continue
-						self.feature[kth][_] = round(np.mean(newY), 4)
-					continue
 
 				if max(newX) - min(newX) + 1 != len(newX): # missing value between min and max
 					grid_x = np.linspace(min(newX), max(newX), max(newX) - min(newX) + 1)
@@ -133,7 +133,7 @@ class preprocessing:
 		self.feature_name = np.array(self.feature_name)
 	
 	def calculateRisk(self):
-		print "Compute stock returns and CVAR"
+		print "Compute stock risk ratios"
 		self.returns, self.DR, self.SD = {}, {}, {} # DR: downside deviation
 		self.CVAR = {95:{}, 99:{}, 99.9:{}}
 		for ticker in self.stock_prices:
@@ -149,46 +149,40 @@ class preprocessing:
 					last = date
 			returns_risk = np.array(self.returns[ticker].values())
 			returns_risk = returns_risk[~np.isnan(returns_risk)] # delete missing value
-			for alpha in [0.1, 1, 5]: # change to percentile alpha
-				VaR = np.percentile(returns_risk, alpha)
-				if len(returns_risk[returns_risk < VaR]) == 0: continue
-				#self.CVAR[100 - alpha][ticker] = np.mean(returns_risk[returns_risk < VaR])
-				self.DR[ticker] = np.std(returns_risk[returns_risk <= 0]) * np.sqrt(252)
-				#self.SD[ticker] = np.std(returns_risk) * np.sqrt(252) # annualized
+			self.DR[ticker] = np.std(returns_risk[returns_risk <= 0]) * np.sqrt(252)
+			# for alpha in [0.1, 1, 5]: # change to percentile alpha
+			# 	VaR = np.percentile(returns_risk, alpha)
+			# 	if len(returns_risk[returns_risk < VaR]) == 0: continue
+			# 	#self.CVAR[100 - alpha][ticker] = np.mean(returns_risk[returns_risk < VaR])
+			# 	self.DR[ticker] = np.std(returns_risk[returns_risk <= 0]) * np.sqrt(252)
+			# 	#self.SD[ticker] = np.std(returns_risk) * np.sqrt(252) # annualized
 			
 	def createLabel(self):
-		print "Create Label Based on sharpe ratio"
+		print "Create Label Based on Sortino ratio"
 		self.label = {}
 		for name in ["train0", "train1", "train2", "train3", "test"]:
-			self.label[name] = np.zeros(len(self.feature), dtype=int) # to train model
+			self.label[name] = np.zeros(len(self.feature)) # to train model
 		if len(self.feature) != len(self.tickerList): 
 			sys.exit("feature number doesn't match label information")
-		self.deleteList = {}
+
 		risk_free = 0.016 # USD LIBOR - 12 months
+		def calcRatio(date0, date1, type, ticker, mul):
+			if date0 in self.stock_prices[ticker] and date1 in self.stock_prices[ticker]:
+				annualized_r = self.stock_prices[ticker][date1] / \
+							self.stock_prices[ticker][date0] - 1			
+				# Sortino ratio is better to evalueate high-volatility portfolio
+				Sortino_ratio = (annualized_r - risk_free) / self.DR[ticker] * mul
+				#Sharpe_ratio = (annualized_r - risk_free) / self.SD[ticker] * mul
+				self.label[type][_] = round(Sortino_ratio, 1)
+			else:
+				self.label[type][_] = np.nan
 		for _ in xrange(len(self.tickerList)):
 			ticker = self.tickerList[_]
-			try: # some company may have not IPO yet
-				def calcRatio(date0, date1, type, mul):
-					annualized_r = self.stock_prices[ticker][date1] / \
-								self.stock_prices[ticker][date0] - 1			
-					# Sortino ratio is better to evalueate high-volatility portfolio
-					Sortino_ratio = (annualized_r - risk_free) / self.DR[ticker] * mul
-					#Sharpe_ratio = (annualized_r - risk_free) / self.SD[ticker] * mul
-
-					if Sortino_ratio >= 1:
-						self.label[type][_] = 1
-					# if type == "train3":
-					# 	print("%6s\tSortino: %4s\t\tCVaR 95%%: %5s%%\t%d" % \
-					# 		(ticker, str(round(Sortino_ratio, 1))[:5], \
-					# 		str(self.CVAR[95][ticker] * 100)[:5], self.label["train3"][_]))
-				calcRatio("2012-01-03", "2012-03-01", "train0", 252. / 40)# 40 days
-				calcRatio("2013-01-02", "2013-03-01", "train1", 252. / 40) # 40 days
-				calcRatio("2014-01-02", "2014-03-03", "train2", 252. / 40) # 40 days
-				calcRatio("2015-01-02", "2015-03-03", "train3", 252. / 40) # 40 days
-				calcRatio("2016-01-04", "2016-03-02", "test", 252. / 40) 
-			except:
-				self.deleteList[ticker] = None # delete unqualified stock
-				continue
+			calcRatio("2012-01-03", "2012-03-01", "train0", ticker, 252. / 40)# 40 days
+			calcRatio("2013-01-02", "2013-03-01", "train1", ticker, 252. / 40) # 40 days
+			calcRatio("2014-01-02", "2014-03-03", "train2", ticker, 252. / 40) # 40 days
+			calcRatio("2015-01-02", "2015-03-03", "train3", ticker, 252. / 40) # 40 days
+			calcRatio("2016-01-04", "2016-03-02", "test", ticker, 252. / 40) # 40 days
 
 	def cleanFeatures(self):
 		print "\nClean Features"
@@ -209,16 +203,16 @@ class preprocessing:
 		# tag true with feature variance is above than threshold, otherwise tag false
 		#print len((np.std(self.feature[~np.isnan(self.feature)], axis=0) > 0))
 		#tag_sd = (np.std(self.feature.astype(np.float), axis=0) > 0) # this can't deal with nan
-		tag_sd = np.repeat(True, np.shape(self.feature)[1])
-		for num in range(np.shape(self.feature)[1]):
-			std = np.std(self.feature[~np.isnan(self.feature[:,num]), num])
-			#mean = np.mean(self.feature[~np.isnan(self.feature[:,num]), num])
-			if std == 0: tag_sd[num] = False
-		self.feature = self.feature[:,tag_sd]
-		self.feature_name = self.feature_name[tag_sd]
-		print "Feature dimension after variance check: ", np.shape(self.feature)
+		# tag_sd = np.repeat(True, np.shape(self.feature)[1])
+		# for num in range(np.shape(self.feature)[1]):
+		# 	std = np.std(self.feature[~np.isnan(self.feature[:,num]), num])
+		# 	#mean = np.mean(self.feature[~np.isnan(self.feature[:,num]), num])
+		# 	if std == 0: tag_sd[num] = False
+		# self.feature = self.feature[:,tag_sd]
+		# self.feature_name = self.feature_name[tag_sd]
+		# print "Feature dimension after variance check: ", np.shape(self.feature)
 
-		# if a ratio in features doesn't have 11 data, we can't get time-window data
+		# if a ratio in features doesn't have 11 data, we can't get time-window shift
 		# delete all the related ratios asscociated with that
 		tag_full_ratio = np.repeat(True, np.shape(self.feature)[1])
 		last = -1; cnt = 0
@@ -237,16 +231,26 @@ class preprocessing:
 		print 'Feature dimension after ratio-time completeness check:', \
 			np.shape(self.feature)
 
-
 		# do data imputation to fill in missing values
+		# load file of feature_name to determine which feature to scale
 		imp = Imputer(missing_values='NaN', strategy='median', axis=0)
 		imp.fit(self.feature)
 		self.feature = imp.transform(self.feature)
+
 		# feature scaling, can't do it in createFeature part due to missing values
 		fclasses = len(self.feature[0]) / 11
 		for num in range(len(self.feature)):
 			feature_trans = np.reshape(self.feature[num], [fclasses, 11])
-			feature_trans = scale(feature_trans, axis=1).round(3)
+			#feature_trans = scale(feature_trans, axis=1).round(3)
+			# we should scale the non-percent values
+			for fnum in range(fclasses):
+				featureName = "key_ratios_" + self.feature_name[fnum].split("__")[0]
+				featureType = self.wordDict[featureName]
+				if featureType == "scalar": # transform to scale number
+					feature_trans[fnum] = scale(feature_trans[fnum]).round(3)
+				elif featureType == "percent": # transform xy% to 0.xy
+					feature_trans[fnum] = feature_trans[fnum] / 100.
+				else: continue # actually, we should not go into this loop
 			self.feature[num] = feature_trans.flatten()
 
 		# add ticker to the 1st col, label to the last col of the feature matrix
@@ -259,19 +263,19 @@ class preprocessing:
 		print "Feature dimension after col-merge: ", np.shape(self.feature)
 
 		# if the stock don't have coresponding price, delete it.
-		tag_price = np.repeat(True, len(self.feature))
-		for num in xrange(len(self.feature)):
-			if self.feature[num][0] in self.deleteList:
-				tag_price[num] = False
-		self.feature = self.feature[tag_price]
-		print "Feature dimension after price check: ", np.shape(self.feature)
+		# tag_price = np.repeat(True, len(self.feature))
+		# for num in xrange(len(self.feature)):
+		# 	if self.feature[num][0] in self.deleteList:
+		# 		tag_price[num] = False
+		# self.feature = self.feature[tag_price]
+		# print "Feature dimension after price check: ", np.shape(self.feature)
 
 
 	def saveLocal(self):
-		np.savetxt("./dat/feature_label_" + date_start + '_' + date_end, \
+		np.savetxt("./input/feature_label_" + date_start + '_' + date_end, \
 			self.feature, delimiter=',', fmt="%s")
 
-		np.savetxt("./dat/selected_feature_" + date_start + '_' + date_end, \
+		np.savetxt("./output/selected_feature_" + date_start + '_' + date_end, \
 			self.feature_name, delimiter=',', fmt="%s")
 
 
--- learning.py
+++ learning.py
@@ -7,65 +7,73 @@ from datetime import datetime
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import scale
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from collections import Counter
 
-# from sklearn document
-# Proper choice of C and gamma is critical to the SVM performance. 
-# use sklearn.model_selection.GridSearchCV with C and gamma 
-# spaced exponentially far apart to choose good values.
-# http://scikit-learn.org/stable/modules/generated/sklearn.model_\
-# selection.GridSearchCV.html#sklearn.model_selection.GridSearchCV
-
-
-# should update: add another label, one to train model, one to make prediction
+# no sector, matketCap feature right now
 
 class learning:
-	def __init__(self, date_start, date_end, date_type):
+	def __init__(self, date_start, date_end, date_type, sortino_rate, testYear):
 		self.loadFile()
 		self.dataProcressing()
 		#self.SVM()
 		self.neuralNet()
+		self.boosting()
+		self.randomForest()
 
 	def loadFile(self):
 		typePar = "_".join([date_start, date_end])
 		# load feature names
-		self.feature_names = np.loadtxt("./dat/selected_feature_" + typePar, dtype='str')
+		self.feature_names = np.loadtxt("./output/selected_feature_" + typePar, dtype='str')
 		# load feature_label file, 1st col as tickers, last col as label
-		dat = np.loadtxt("./dat/feature_label_" + typePar, delimiter=',', dtype='str')
+		dat = np.loadtxt("./input/feature_label_" + typePar, delimiter=',', dtype='str')
 		self.tickers = dat[:,0]
 		self.feature = dat[:, 1:np.shape(dat)[1] - 5].astype(np.float)
-		self.label_train = {}
-		for i in range(4):
-			self.label_train[i] = dat[:,-5 + i].astype(np.int)
-		self.label_test = dat[:,-1].astype(np.int)
+		self.label = {}
+		for i in range(5): # the last 5 cols are labels in the most recent 5 years
+			self.label[i] = dat[:,-5 + i].astype(np.float)
+		
+		self.fticker = open('./output/tickers_' + "_".join([date_start, date_end]), 'w')
+		self.fout = open('./output/result_' + "_".join([date_start, date_end]), 'a+') # save to local
 
-		imp = Imputer(missing_values='NaN', strategy='median', axis=0)
-		imp.fit(self.feature)
-		self.feature = imp.transform(self.feature)
-
-		self.fout = open('./dat/result_' + "_".join([date_start, date_end]), 'a+') # save to local
-		self.fticker = open('./dat/tickers_' + "_".join([date_start, date_end]), 'w')
 		curtime = str(datetime.now())
-		self.ptLocal(self.fout, "Today: %s",curtime[:len(curtime) - 10])
 
+		self.ptLocal(self.fout, "\n\nToday: %s",curtime[:len(curtime) - 10])
+		self.ptLocal(self.fout, "Training data based on period: %s", \
+			"2006-" + str(testYear - 1))
+
+	# both print to screen and output to file
 	def ptLocal(self, fout, content, pars): # pars should be an array
 		print(content % (pars))
 		content += "\n"
 		fout.write(content % (pars))
 
 	def dataProcressing(self):
-		# train model use data from 2006-2011, 2007-12, 2008-13, 2009-14
-		for i in range(4):
-			tag = self.selectedTime(self.feature_names, 2006 + i, 2011 + i, 0)
-			if i == 0: self.X, self.y = self.feature[:, tag], self.label_train[i]
-			else:
-				self.X = np.vstack([self.X, self.feature[:, tag]])
-				self.y = np.vstack([self.y, self.label_train[i]])
-		self.y = self.y.flatten()
-		print np.shape(self.X), np.shape(self.y)
+		# increase training data by combining time horizon
+		# 2006-2011, 2007-2012, 2008-2013, 2009-2014, 2010-2015, excluding TTM
+		startYear = 2011
+		for i in range(startYear, testYear):
+			# tag time window to shift, increase the training data
+			tag0 = self.selectedTime(self.feature_names, i - 5, i, 0)
+			num = i - startYear # corresponds to the last 5 cols
+			tag1 = ~np.isnan(self.label[num]) # tag non-None value as true
+			if i == startYear: # 1st col in label taken as initialization
+				self.X, self.y = self.feature[tag1][:,tag0], self.label[num][tag1]
+			elif i < testYear - 1:
+				self.X = np.vstack([self.X, self.feature[tag1][:,tag0]])
+				self.y = np.concatenate([self.y, self.label[num][tag1]])
+			elif i == testYear - 1: # last col taken as test data
+				self.X_test, self.expected = self.feature[tag1][:,tag0], self.label[num][tag1]
+		
+		# define our label 1 if the sortino ratio is higher than the threshold
+		self.y[self.y < sortino_rate], self.expected[self.expected < sortino_rate] = 0, 0
+		self.y[self.y >= sortino_rate], self.expected[self.expected >= sortino_rate] = 1, 1
+		self.y, self.expected = self.y.astype(int), self.expected.astype(int)
 		# sample number
 		n_samples = len(self.tickers)
 		self.ptLocal(self.fout, "Feature dimension (sample, training feature): %d %d\n", \
@@ -73,16 +81,12 @@ class learning:
 		# count the number of each label
 		label_cnt = Counter(self.y)
 		for _ in label_cnt:
-			self.ptLocal(self.fout, "Label %d: number %d", (_, label_cnt[_]))
-
-		# make prediction based on data from 2010-2015, excluding TTM
-		tag = self.selectedTime(self.feature_names, 2010, 2015, 0)
-		self.X_test, self.expected = self.feature[:, tag], self.label_test
+			self.ptLocal(self.fout, "Label %d number: %d", (_, label_cnt[_]))
+		self.ptLocal(self.fout, "Random pick successful rate: %.3f\n",\
+			round(float(sum(self.expected)) / len(self.expected), 3))
 		# cross validation
 		self.K = 3
 		self.k_fold = KFold(self.K)
-		
-
 
 	def selectedTime(self, names, year0, year1, ifTTM): # year1 as int
 		tag = np.repeat(True, len(names))
@@ -127,7 +131,7 @@ class learning:
 		print "Random pick successful ratio: ", \
 			round(float(sum(self.expected)) / len(self.expected), 3)
 
-	def neuralNet(self):
+	def allNeuralNet(self):
 		def combine(n, k): # generate Combination 
 			if k == 1:
 				return [[i] for i in range(1, n+1)]
@@ -144,22 +148,24 @@ class learning:
 				layer.insert(layer_n, 2)
 				score = [0] * self.K
 				for k, (train, test) in enumerate(self.k_fold.split(self.X, self.y)):
-					clf = MLPClassifier(solver='lbfgs', alpha=1e-5,\
-						hidden_layer_sizes=layer, random_state=1)
+					clf = MLPClassifier(solver='lbfgs', alpha=1e-5, early_stopping=True, \
+						hidden_layer_sizes=layer, random_state=1, momentum=0.5, \
+						max_iter=10 ** 5)
 					clf.fit(self.X[train], self.y[train])
 					#score[k] = clf.score(self.X[test], self.y[test]) # if we care the whole
 					predicted = clf.predict(self.X[test])
 					score_mat = metrics.confusion_matrix(self.y[test], predicted)
 
-					if score_mat[1, 1] == 0: continue # only care label-1 performance
+					if score_mat[1, 1] < 30: continue # only care label-1 performance
 					recall = float(score_mat[1, 1]) / sum(score_mat[1])
 					precision = float(score_mat[1, 1]) / sum(score_mat[:, 1])
-					score[k] = 1 / ((1 / recall + 1 / precision) / 2) # f1 score
+					#score[k] = 1 / ((1 / recall + 1 / precision) / 2) # f1 score
+					score[k] = precision
 				#if min(score) <= best: continue # only update if we have better result
 				best, idx = min(score), score.index(min(score))
 				self.ptLocal(self.fout, "\nLayers: %s", ("-".join(np.array(layer, dtype=str))))
 				self.ptLocal(self.fout, "Confusion matrix:\n%s", score_mat)
-				self.ptLocal(self.fout, "Label 1 expected f1 score: %.3f\n", best)
+				self.ptLocal(self.fout, "Label 1 expected precision: %.3f\n", best)
 				clf.fit(self.X, self.y)
 				predicted = clf.predict(self.X_test)
 				self.ptLocal(self.fout, "Classification report for classifier %s:\n%s", \
@@ -172,9 +178,39 @@ class learning:
 				self.ptLocal(self.fout, "Random pick successful rate: %.3f\n",\
 				 round(float(sum(self.expected)) / len(self.expected), 3))
 
+	def neuralNet(self):
+		self.ptLocal(self.fout, "\nDeep Neural Network%s", "")
+		layer = [168, 32, 2]
+		self.clf = MLPClassifier(solver='lbfgs', alpha=1e-5, early_stopping=True, \
+			hidden_layer_sizes=layer, random_state=0, momentum=0.5, \
+			max_iter=10 ** 5)
+		self.perform()
+
+	def randomForest(self):
+		self.ptLocal(self.fout, "\nRandom Forest%s", "")
+		self.clf = RandomForestClassifier(random_state=0)
+		self.perform()
+
+	def boosting(self):
+		self.ptLocal(self.fout, "\nGradient Boosting%s", "")
+		self.clf = GradientBoostingClassifier(n_estimators=1000, random_state=0)
+		self.perform()
+
+	def perform(self):
+		self.clf.fit(self.X, self.y)
+		scores = cross_val_score(self.clf, self.X, self.y).mean()
+		self.ptLocal(self.fout, "Cross Validation Score: %.3f", scores)
+		predicted = self.clf.predict(self.X_test)
+		self.ptLocal(self.fout, "Classification report for classifier %s:\n%s", \
+			(self.clf, metrics.classification_report(self.expected, predicted)))
+		self.ptLocal(self.fout, "Confusion matrix:\n%s", \
+			metrics.confusion_matrix(self.expected, predicted))
+		
 
 if __name__ == "__main__":
 	date_start = "2000-01-01"
 	date_end = "2016-12-31"
-	date_type = "d"
-	s = learning(date_start, date_end, date_type)
+	date_type = "d" # daily data
+	sortino_rate = .5 # set classification threshold
+	testYear = 2015
+	s = learning(date_start, date_end, date_type, sortino_rate, testYear)
